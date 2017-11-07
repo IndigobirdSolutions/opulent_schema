cd566acbc76c8f5d8529d76730c829b5b41b35d1
mpnowacki <michal@nowacki.codes>
2017-11-07 11:36:28 +0100

    first version of setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import atexit
+
+from setuptools import setup
+
+version = "0.1.0"
+
+setup(
+    name='opulent_schema',
+    url='https://github.com/4SmartIT/opulent_schema',
+    version=version,
+    license='BSD',
+    platforms=['any'],
+    packages=['opulent_schema'],
+    install_requires=[
+        "voluptuous",    
+    ],
+    extra_require={
+        'extra': [
+            'delorean',
+            'sqlalchemy',
+        ],    
+    },
+)
